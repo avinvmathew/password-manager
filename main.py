99b15f8e8abeded8f270ae7423b924b4aa6bf84b
Avin Varghese Mathew <avinvmavolil@gmail.com>
2025-07-05 20:38:23 +0530

    second commit

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from tkinter import *
+from datetime import datetime
+from tkinter import messagebox
+from PassGenerator import PassGen
+import pyperclip
+
 FONT = ("Comic Sans MS", 10)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate_password():
+    password_generator = PassGen()
+    password_entry.delete(0,END)
+    new_pwd = password_generator.generate_password()
+    pyperclip.copy(new_pwd)
+    password_entry.insert(index=0, string=new_pwd)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+#writing to file
+def save_to_file():
+    current_datetime = datetime.now()
+    website_1 = website_entry.get()
+    username_2 = username_entry.get()
+    password_3 = password_entry.get()
+
+    if len(website_1) == 0 or len(username_2) == 0 or len(password_3) == 0:
+        messagebox.showinfo(title="Fields empty!", message="Please don't leave any fields empty!")
+    else:
+        is_user_ok = messagebox.askokcancel(title=f"{website_1}", message=f"Please confirm your entry!\nUsername/email: {username_2}\nPassword: {password_3}")
+        if is_user_ok:
+            with open("pwd_data.txt", "a+") as file:
+                file.write(f"Website: {website_1} | Username/Email: {username_2} | Password: {password_3} => added on/at : {current_datetime}\n")
+            print("Password saved!")
+            website_entry.delete(0,END)
+            # username_entry.delete(0,END)
+            password_entry.delete(0,END)
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Password Manager")
 window.option_add("*Font",FONT)
@@ -34,18 +66,16 @@ username_entry = Entry(width=35)
 username_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
 username_entry.insert(0,"mandalorian@gmail.com")
 
+# password_entry = Entry(width=21, show="*")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="ew")
 
 #Buttons
-generate_pwd_button = Button(text="Generate Password")
+generate_pwd_button = Button(text="Generate Password", command=generate_password)
 generate_pwd_button.grid(row=3, column=2, sticky="ew")
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_to_file)
 add_button.grid(row=4, column=1, columnspan=2, sticky="ew")
-
-#entry acquisition and writing to file
-
 
 
 
